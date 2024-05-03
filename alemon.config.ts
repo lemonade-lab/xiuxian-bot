2c7b8c543603733935a9d11cbdc925e3ffe18e08
ningmengchongshui <916415899@qq.com>
2024-05-04 03:01:51 +0800

    fix: 修复

--- alemon.config.ts
+++ alemon.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'alemonjs'
 const scripts =
   process.env.NODE_ENV == 'production' ? 'dist/index.js' : 'src/main.ts'
-console.info('[APP] 凡人修仙 启动', scripts)
+
+console.info('[APP] 凡人修仙 启动', process.env.NODE_ENV, scripts)
 export default defineConfig({
   app: {
     scripts
