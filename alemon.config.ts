2a3032f1a1795cd4ce391bb1f080849766d1aee5
ningmengchongshui <916415899@qq.com>
2024-05-04 03:17:24 +0800

    fix: 修改打印

--- alemon.config.ts
+++ alemon.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'alemonjs'
-const scripts =
-  process.env.NODE_ENV == 'production' ? 'dist/index.js' : 'src/main.ts'
-
-console.info('[APP] 凡人修仙 启动', process.env.NODE_ENV, scripts)
+const NODE_ENV = process.env.NODE_ENV
+const scripts = NODE_ENV == 'production' ? 'dist/index.js' : 'src/main.ts'
+console.info('[APP] 凡人修仙 启动', NODE_ENV, scripts)
 export default defineConfig({
   app: {
     scripts
