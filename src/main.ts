a9b0b4d57769a4e6770e0bdcb35244a992a0eeb3
ningmengchongshui <916415899@qq.com>
2024-05-05 15:22:49 +0800

    feat: 测试

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { createApp } from 'alemonjs'
 import * as apps from './apps.js'
 import * as test from './app-test.js'
-import { event } from './rules.js'
+// import { event } from './rules.js'
 const app = createApp(import.meta.url)
 if (process.env.APP_TEXT == '0') {
   app.use(test)
 } else {
   app.use(apps)
-  app.use(event.ok())
+  // app.use(event.ok())
 }
 app.mount()
