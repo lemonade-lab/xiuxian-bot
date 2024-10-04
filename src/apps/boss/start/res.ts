af65c244be32f3701b78da1bcda0bc0e48f2acf6
xiejihuang@baidu.com <xiejihuang@baidu.com>
2024-10-04 17:59:58 +0800

    feat: 测试boss2

--- src/apps/boss/start/res.ts
+++ src/apps/boss/start/res.ts
@@ -27,6 +27,12 @@ export default OnResponse(
       return
     }
 
+    if (bossInfo.data.battle_blood_now <= 1) {
+      Send(Text('BOSS复活中....'))
+      Boss.updateBossData(key)
+      return
+    }
+
     //
     try {
       const Now = new Date()
