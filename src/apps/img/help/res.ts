e82b835f59052a9099098b193918dc4a870554a4
xiejihuang@baidu.com <xiejihuang@baidu.com>
2024-10-05 15:32:40 +0800

    feat: 调整

--- src/apps/img/help/res.ts
+++ src/apps/img/help/res.ts
@@ -1,7 +1,7 @@
 import { Image, useParse, useSend } from 'alemonjs'
 import { readFileSync } from 'fs'
 import { join } from 'path'
-import { pictureRender } from '@/xiuxian/img/src/index.js'
+import { pictureRender } from '@xiuxian/img/index'
 const helpData = {}
 const dir = join(process.cwd(), 'public', 'defset', 'base_help.json')
 export default OnResponse(
